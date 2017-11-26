cb21a3a6781903068af84caf5e99515cfbf9b178
Your Name <you@example.com>
2017-11-26 03:21:10 +0200

    init

---
+++ src/main/java/org/home/apapacy/models/PersonModel.java
@@ -0,0 +1,67 @@
+package org.home.apapacy.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity bean with JPA annotations
+ * Hibernate provides JPA implementation
+ * @author pankaj
+ *
+ */
+@Entity
+@Table(name="PERSON")
+public class PersonModel {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String country;
+
+	private String memo;
+	
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Override
+	public String toString(){
+		return "id="+id+", name="+name+", country="+country;
+	}
+}
